f6e0d60b2c42fbe2ec7d082abb4131f7279cb37c
Kamyab Zareh <kamyab.zareh@gmail.com>
2018-08-19 10:14:04 +0430

    #20 add user model

---
+++ authentication/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+from authentication.models import User
+
+admin.site.register(User)
---
+++ authentication/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.base_user import AbstractBaseUser
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=30, unique=True)
+    fullname = models.CharField(max_length=50, blank=True, null=True)
+    profile_picture = models.ImageField(upload_to="profiles/", blank=True, null=True)
+    bio = models.CharField(max_length=300, blank=True, null=True)
+    phone_number = models.CharField(max_length=30, unique=True, blank=True, null=True)
+    register_date = models.DateField(auto_now=True)
+
+    USERNAME_FIELD = 'username'
+    Email_Field = 'email'
+
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return "Username: {}, Email: {}".format(self.username, self.email)
