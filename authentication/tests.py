c42004b6a0c5e6796c522613f5621937c88107b0
Kamyab Zareh <kamyab.zareh@gmail.com>
2018-08-20 14:07:43 +0430

    create user function and write test for it

--- authentication/tests.py
+++ authentication/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from authentication.models import User
+from authentication.utils import create_user
 
-# Create your tests here.
+
+class UserTestCase(TestCase):
+    def setUp(self):
+        self.data = '''
+        {
+            "username": "kamyab",
+            "password": "1",
+            "email": "kamyab@kamyab.com"
+        }
+        '''
+
+    def test_create_user(self):
+        user = create_user(self.data)
+        self.assertEqual(user, User.objects.first())
