b5b57b83336ebfb63b5f8565b8d099dcec475a58
Kamyab Zareh <kamyab.zareh@gmail.com>
2018-08-20 14:54:04 +0430

    add login after signup function and test for it

--- authentication/utils.py
+++ authentication/utils.py
@@ -1,4 +1,7 @@
 import json
+
+from rest_framework_jwt.settings import api_settings
+
 from authentication.models import User
 
 
@@ -38,3 +41,13 @@ def create_user(data):
     user.save()
 
     return user
+
+
+def login_after_signup(user):
+    jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+    jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+
+    payload = jwt_payload_handler(user)
+    token = jwt_encode_handler(payload)
+
+    return token
