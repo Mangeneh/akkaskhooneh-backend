03c95059d4f10e63bd50088306e79222932c22fb
Kamyab Zareh <kamyab.zareh@gmail.com>
2018-08-21 14:20:06 +0430

    create user function and integrate with login #29 #30 #31

--- authentication/views/views.py
+++ authentication/views/views.py
@@ -1,7 +1,7 @@
-from rest_framework import mixins, generics, status, viewsets, permissions
+from rest_framework import generics, permissions
 
 from authentication.models import User
-from authentication.serializers import UserSerializer
+from authentication.serializers.user_serializer import UserSerializer
 
 class register(generics.ListCreateAPIView):
     http_method_names = ['post']
