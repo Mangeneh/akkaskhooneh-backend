73456c4737301a736a04a34dcbc4a2c806e57e8e
Kamyab Zareh <kamyab.zareh@gmail.com>
2018-08-25 14:04:39 +0430

    add not found status code and delete some useless codes

--- social/views/profile_view_set.py
+++ social/views/profile_view_set.py
@@ -1,4 +1,5 @@
 from django.http import Http404
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -11,12 +12,6 @@ class ProfileViewSet(APIView):
     Retrieve a profile instance.
     """
 
-    def get_object(self, pk):
-        try:
-            return User.objects.get(pk=pk)
-        except User.DoesNotExist:
-            raise Http404
-
     def get(self, request, username=None, format=None):
         if username is None:
             user = request.user
@@ -24,5 +19,10 @@ class ProfileViewSet(APIView):
             return Response(serializer.data)
         else:
             user = User.objects.filter(username=username).first()
+
+            if user is None:
+                response_content = {"detail": "User not Found"}
+                return Response(response_content, status.HTTP_404_NOT_FOUND)
+
             serializer = ProfileSerializer(user)
             return Response(serializer.data)
