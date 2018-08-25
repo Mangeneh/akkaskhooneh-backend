e8deed3e34eb20d75d631c22b656c24ddb198979
Rasool Safari <safari@rasooll.com>
2018-08-25 15:57:59 +0430

    Create pagination API for get pictures url

--- social/urls/urls.py
+++ social/urls/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
 
 from social.views.profile_view_set import ProfileViewSet
+from social.views.pagination import PaginationApiView
 
 urlpatterns = [
     path('profile/<str:username>', ProfileViewSet.as_view()),
     path('profile/', ProfileViewSet.as_view()),
+    path('pictures/', PaginationApiView.as_view()),
+    path('pictures/<str:username>', PaginationApiView.as_view()),
 ]
