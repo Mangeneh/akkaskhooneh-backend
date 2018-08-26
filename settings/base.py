5924c972312033abd25966f94f8c52e01086ede4
Rasool Safari <safari@rasooll.com>
2018-08-26 10:34:38 +0430

    Change password validation

--- settings/base.py
+++ settings/base.py
@@ -80,9 +80,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'OPTIONS': {
+            'min_length': 6,
+        }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
