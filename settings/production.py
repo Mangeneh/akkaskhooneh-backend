d0d942c2ba2380ebb6fed57788ea1f070098f358
Rasool Safari <safari@rasooll.com>
2018-08-27 12:49:09 +0430

    edit production settings.

--- settings/production.py
+++ settings/production.py
@@ -8,15 +8,16 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.11.140']
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# TODO Database connection in not configure.
-# TODO Install 'libmysqlclient-dev' package before installing mysqlclient.
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'myproject',
+        'NAME': 'testproject',
         'USER': 'root',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
+        'PASSWORD': 'nX2G7yeRgUf2dcR',
+        'HOST': '172.17.0.2',
         'PORT': '',
     }
 }
+
+MEDIA_ROOT = '/mnt/nginx/media'
+STATIC_ROOT = '/mnt/nginx/static'
