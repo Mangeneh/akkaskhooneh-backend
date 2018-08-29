812a939e1bfa3b352adc69e0216824a7b6ecc924
Rasool Safari <safari@rasooll.com>
2018-08-29 11:55:29 +0430

    Create validator for images size.

--- authentication/models/user.py
+++ authentication/models/user.py
@@ -3,15 +3,26 @@ from django.db import models
 from django.contrib.auth.base_user import AbstractBaseUser
 
 from authentication.managers import UserManager
+from django.core.exceptions import ValidationError
 
 
 class User(AbstractBaseUser, PermissionsMixin):
+
+    def validate_image(fieldfile_obj):
+        filesize = fieldfile_obj.file.size
+        megabyte_limit = 5.0
+        if filesize > megabyte_limit*1024*1024:
+            raise ValidationError("Max file size is %sMB" %
+                                  str(megabyte_limit))
+
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=30, unique=True)
     fullname = models.CharField(max_length=50, blank=True, null=True)
-    profile_picture = models.ImageField(upload_to="profiles/", blank=True, null=True)
+    profile_picture = models.ImageField(
+        upload_to="profiles/", blank=True, null=True, validators=[validate_image])
     bio = models.CharField(max_length=300, blank=True, null=True)
-    phone_number = models.CharField(max_length=30, unique=True, blank=True, null=True)
+    phone_number = models.CharField(
+        max_length=30, unique=True, blank=True, null=True)
     register_date = models.DateField(auto_now=True)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
--- social/models/posts.py
+++ social/models/posts.py
@@ -1,12 +1,20 @@
 from django.db import models
 from authentication.models import User
+from django.core.exceptions import ValidationError
 
 
 class Posts(models.Model):
+
+    def validate_image(fieldfile_obj):
+        filesize = fieldfile_obj.file.size
+        megabyte_limit = 5.0
+        if filesize > megabyte_limit*1024*1024:
+            raise ValidationError("Max file size is %sMB" %
+                                  str(megabyte_limit))
+
     owner = models.ForeignKey(
-        User, on_delete=models.CASCADE,
-        related_name="owner"
-    )
-    picture = models.ImageField(upload_to="post_pictures/")
+        User, on_delete=models.CASCADE, related_name="owner")
+    picture = models.ImageField(
+        upload_to="post_pictures/", validators=[validate_image])
     caption = models.TextField(max_length=1023, blank=True, null=True)
     time = models.DateTimeField(auto_now_add=True)
