c3b8356e06f980856eb0e88cb178230aaa8cef23
Rasool Safari <safari@rasooll.com>
2018-09-02 17:01:57 +0430

    Create new board api

--- social/urls.py
+++ social/urls.py
@@ -4,6 +4,7 @@ from social.views.create_new_post_api_view import CreateNewPostAPIView
 from social.views.profile_view_set import ProfileViewSet
 from social.views.set_profile_view_set import SetProfilePicViewSet
 from social.views.pagination import PaginationApiView
+from social.views.create_board_apiview import CreateNewBoardApiView
 
 urlpatterns = [
     path('profile/<str:username>/', ProfileViewSet.as_view()),
@@ -12,4 +13,5 @@ urlpatterns = [
     path('pictures/', PaginationApiView.as_view()),
     path('pictures/<str:username>/', PaginationApiView.as_view()),
     path('create-new-post/', CreateNewPostAPIView.as_view()),
+    path('create-new-board/', CreateNewBoardApiView.as_view()),
 ]
