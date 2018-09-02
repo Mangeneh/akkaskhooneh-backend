1be2b9efaca0c57cf25ce8c684cacfa7456f80fe
ali.sh <ali9shafiee@gmail.com>
2018-09-02 17:23:13 +0430

    add like and comment model

--- social/models/__init__.py
+++ social/models/__init__.py
@@ -2,4 +2,5 @@ from .followers import Followers
 from .posts import Posts
 from .tags import Tags, TagContains
 from .board import Board, BoardContains
-
+from .like import Like
+from .comment import Comment
---
+++ social/models/comment.py
@@ -0,0 +1,10 @@
+from django.db import models
+from authentication.models import User
+from social.models import Posts
+
+
+class Comment(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_comment')
+    post = models.ForeignKey(Posts, on_delete=models.CASCADE, related_name='post_comment')
+    content = models.CharField(max_length=1000)
