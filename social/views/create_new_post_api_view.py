9b926f07e424bc87a22db1001a0a417e0b591700
Rasool Safari <safari@rasooll.com>
2018-09-03 13:42:36 +0430

    Get tags for new post api view

--- social/views/create_new_post_api_view.py
+++ social/views/create_new_post_api_view.py
@@ -2,10 +2,13 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from social.models import Posts
+from social.models import Posts, Tags, TagContains
 import utils
+import validators
 import logging
 from social.forms import CreateNewPost
+from django.core.exceptions import ValidationError
+
 
 logger = logging.getLogger('social')
 
@@ -23,13 +26,39 @@ class CreateNewPostAPIView(APIView):
         new_post = CreateNewPost(data, request.FILES)
 
         if new_post.is_valid():
-            new_post.save()
+            post = Posts(
+                owner = new_post.cleaned_data.get('owner'),
+                picture = new_post.cleaned_data.get('picture'),
+                caption = new_post.cleaned_data.get('caption')
+                )
+            
+            tags_list = data.get('tags')
+            if tags_list is not None:
+                tags_list = tags_list.split(',')
+                for tag_item in tags_list:
+                    try:
+                        validators.validate_tag(tag_item)
+                    except ValidationError:
+                        return Response(
+                            data={"tags": "Invalid"},
+                            status=status.HTTP_400_BAD_REQUEST)
+                    tag,_ = Tags.objects.get_or_create(name=tag_item)
+                    
+                post.save()
+
+                for tag_item in tags_list:
+                    tag = Tags.objects.get(name=tag_item)
+                    TagContains.objects.create(tag=tag, post=post)
+            else:
+                post.save()
 
             logger.info('CreateNewPostAPIView: post '
-                '(created successfully) username:{}, ip: {}'.format(
-                    request.user.username, ip))
+                    '(created successfully) username:{}, ip: {}'.format(
+                        request.user.username, ip))
 
-            return Response(status=status.HTTP_201_CREATED)
+            return Response(
+                data={"Result": "OK"},
+                status=status.HTTP_201_CREATED)
         else:
             data_response = {}
             if new_post.errors.get('picture'):
