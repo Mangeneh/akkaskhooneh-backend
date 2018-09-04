834fc29cdabb9fd9bb53bc5af21fee1e98987983
ali.sh <ali9shafiee@gmail.com>
2018-09-04 10:42:39 +0430

    add fullname to search

--- social/views/user_search_api_view.py
+++ social/views/user_search_api_view.py
@@ -1,10 +1,15 @@
+from functools import reduce
+
+from django.db.models import Q
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from operator import and_, or_
 
 import utils
 from authentication.models import User
 from social.serializers.user_search_serializer import UserSearchSerializer
 from settings.base import MEDIA_URL
+
 
 class UserSearchApiView(APIView):
 
@@ -17,11 +22,13 @@ class UserSearchApiView(APIView):
 
         page = request.GET.get('page')
         search_value = request.GET.get('search')
+        search_array = search_value.split(' ')
 
         if page is None:
             page = 1
 
-        data = User.objects.filter(username__icontains=search_value).order_by('-id')
+        data = User.objects.filter(Q(username__icontains=search_value) | Q(reduce(or_, [Q(fullname__icontains=q) for q in search_array]))).order_by('-id')
+
         url = str(request.scheme) + '://' + request.get_host() + MEDIA_URL
         serializer = UserSearchSerializer(self.request.user, context={'page': page, 'url': url, 'data': data,'request_user': self.request.user})
         return Response(serializer.data)
