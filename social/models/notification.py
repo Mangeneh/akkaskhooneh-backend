a0a021c6c07ef9e898d3060406d4b0304a41bc00
Kamyab Zareh <kamyab.zareh@gmail.com>
2018-09-09 16:52:49 +0430

    add notification mdoels #96

--- social/models/notification.py
+++ social/models/notification.py
@@ -3,25 +3,9 @@ from authentication.models import User
 from social.models import Posts
 
 
-class NotificationLike(models.Model):
-    subject_user = models.ForeignKey(User, on_delete=models.CASCADE)
-    target_post = models.ForeignKey(Posts, on_delete=models.CASCADE)
-
-    class Meta:
-        unique_together = ('subject_user', 'object_post')
-
-
-class NotificationFollow(models.Model):
-    subject_user = models.ForeignKey(User, on_delete=models.CASCADE)
-    target_user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    class Meta:
-        unique_together = ('subject_user', 'object_user')
-
-
-class NotificationFollowRequest(models.Model):
-    subject_user = models.ForeignKey(User, on_delete=models.CASCADE)
-    target_user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    class Meta:
-        unique_together = ('subject_user', 'object_user')
+class Notification(models.Model):
+    subject_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='subject_user')
+    target_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='target_user')
+    action_type = models.IntegerField()
+    action_data = models.CharField(max_length=2048)
+    time = models.DateTimeField(auto_now_add=True)
