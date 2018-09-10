133e0e917fd54891227e388952355342c3755ae0
Rasool Safari <safari@rasooll.com>
2018-09-10 10:20:39 +0430

    Adding ci.

---
+++ settings/ci.py
@@ -0,0 +1,20 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+
+DEBUG = True
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'backend',
+        'USER': 'akkaskhooneh',
+        'PASSWORD': 'akkaskhooneh',
+        'HOST': 'mysql',
+        'PORT': '',
+    }
+}
