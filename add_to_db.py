c68787fa944d2da7381d1aac5b7002a97bd3855c
ali.sh <ali9shafiee@gmail.com>
2018-09-11 13:46:29 +0430

    add save notif api

--- add_to_db.py
+++ add_to_db.py
@@ -1,14 +1,7 @@
 import redis
 import json
-from message_queue.utils import redis_config
-import os, django
+from message_queue.utils import redis_config, NotifType
 import requests
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.development")
-django.setup()
-
-from social.models import Notification
-from message_queue.utils import NotifType
 
 r = redis.Redis(**redis_config)
 
@@ -19,8 +12,7 @@ def run():
         ok_data = json_data[1].decode('utf-8')
         data = json.loads(ok_data)
 
-        requests.post('http://localhost:8000', data=data)
-
+        requests.post('http://localhost:8000/social/save/notif/', data=data)
 
 if __name__ == '__main__':
     run()
