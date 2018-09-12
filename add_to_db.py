8ddab5b3718266120237ca086b554cfc8d116c44
Kamyab Zareh <kamyab.zareh@gmail.com>
2018-09-12 14:51:43 +0430

    add log for add_to_db

--- add_to_db.py
+++ add_to_db.py
@@ -1,7 +1,9 @@
 import redis
 import json
-from message_queue.utils import redis_config, NotifType
+from message_queue.utils import redis_config, LogWrapper, _logger
 import requests
+
+logger = LogWrapper(_logger)
 
 r = redis.Redis(**redis_config)
 
@@ -12,8 +14,12 @@ def run():
         ok_data = json_data[1].decode('utf-8')
         data = json.loads(ok_data)
 
+        logger.info("data: {}".format(ok_data))
 
-        req =requests.post('http://localhost:8000/social/save/notif/', json=data)
+        req = requests.post('http://localhost:8000/social/save/notif/', json=data)
+
+        logger.info("data: {}, status: {}".format(ok_data, req.status_code))
+
 
 if __name__ == '__main__':
     run()
--- message_queue/utils.py
+++ message_queue/utils.py
@@ -1,8 +1,8 @@
 from enum import Enum
 import os
+import logging
 
 os.environ.setdefault('REDIS_ADDRESS', 'localhost')
-
 
 redis_config = {
     'host': os.environ.get('REDIS_ADDRESSS'),
@@ -18,3 +18,36 @@ class NotifType(Enum):
     UNLIKE = 5
     UNFOLLOW = 6
     UNREQUEST = 7
+
+
+_logger = logging.getLogger('root')
+FORMAT = " %(levelname)s %(asctime)s %(message)s"
+logging.basicConfig(format=FORMAT)
+_logger.setLevel(logging.DEBUG)
+
+
+class LogWrapper():
+
+    def __init__(self, logger):
+        self.logger = logger
+
+    def info(self, *args, sep=' '):
+        self.logger.info(sep.join("{}".format(a) for a in args))
+
+    def debug(self, *args, sep=' '):
+        self.logger.debug(sep.join("{}".format(a) for a in args))
+
+    def warning(self, *args, sep=' '):
+        self.logger.warning(sep.join("{}".format(a) for a in args))
+
+    def error(self, *args, sep=' '):
+        self.logger.error(sep.join("{}".format(a) for a in args))
+
+    def critical(self, *args, sep=' '):
+        self.logger.critical(sep.join("{}".format(a) for a in args))
+
+    def exception(self, *args, sep=' '):
+        self.logger.exception(sep.join("{}".format(a) for a in args))
+
+    def log(self, *args, sep=' '):
+        self.logger.log(sep.join("{}".format(a) for a in args))
